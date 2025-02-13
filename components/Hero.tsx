7def9fcdd7ed9e9fd046a0b78a209fa57320b88d
Panagiotis Kontoeidis <panos.kontoeidis@gmail.com>
2025-02-13 02:01:28 +0200

    added magicButton

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { SparklesCore } from './ui/sparkles'
-import { TextGenerateEffect } from './ui/text-generate-effect'
+import { TextGenerateEffect } from './ui/textGenerateEffect'
+import MagicButton from './ui/magicButton'
+import { FiDownload } from "react-icons/fi";
+
 
 const Hero = () => {
   return (
     <div className="">
       <div className="h-screen relative w-full bg-black flex flex-col overflow-hidden rounded-md fade-mask">
         <div className="md:text-6xl text-3xl lg:text-6xl font-bold text-center text-white pt-20 mt-20">
-          <p > 
-            Hello, I am Panagiotis
-          </p>
-          <TextGenerateEffect words="I am a Software Developer" className="text-3xl" />
+          <p>Hello, I&apos;m Panagiotis</p>
+          <TextGenerateEffect
+            words="Welcome to my portofolio"
+            className="text-3xl"
+          />
         </div>
-        <div className="w-full absolute inset-0 h-screen items-center justify-center">
+        <div className="w-full absolute inset-0 h-screen items-center justify-center mb-4">
           <SparklesCore
             id="tsparticlesfullpage"
             background="transparent"
@@ -23,6 +27,9 @@ const Hero = () => {
             particleColor="#FFFFFF"
           />
         </div>
+        <a href="#about" className='items-center justify-center flex'>
+          <MagicButton title="Download my CV" icon={<FiDownload/>} position="right"/>
+        </a>
       </div>
     </div>
   );
