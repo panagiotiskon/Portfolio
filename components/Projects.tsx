703e3d053ebbb7fba2e4e507286c34f02989fa34
Panagiotis Kontoeidis <panos.kontoeidis@gmail.com>
2025-03-26 23:24:24 +0200

    finished project images

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,12 +1,15 @@
 import SectionHeader from './SectionHeader';
 import { BentoGrid, BentoGridItem } from './ui/bentoGrid';
+import Image from 'next/image';
 
 const Skeleton = ({ imageSrc, otherClassNames = '' }) => (
   <div className="relative flex w-full h-full min-h-[6rem] rounded-xl overflow-hidden bg-transparent">
-    <img
+    <Image
       src={imageSrc}
-      alt="Project Preview"
-      className={`absolute top-[-10px] right-0 object-contain rounded-xl w-max h-max rounded-xl ${otherClassNames}`}
+      alt="Project Image"
+      className={`absolute object-contain self-center ${otherClassNames}`}
+      layout="fill"
+      objectFit="contain"
     />
   </div>
 );
@@ -16,7 +19,7 @@ const items = [
     link: 'https://github.com/panagiotiskon/Connect-In',
     description:
       'LinkedIn-style professional networking application that allows users to create profiles, manage connections, and interact with other professionals. The application features user role management, job listings, private messaging, and personalized recommendations for job postings and content through Matrix Factorization Algorithm.',
-    header: <Skeleton imageSrc={'/connectIn.jpg'} otherClassNames="" />,
+    header: <Skeleton imageSrc={'/ConnectIn.png'} />,
     className: 'md:col-span-2',
     skills: [
       { skillName: 'SpringBoot', skillURL: 'https://spring.io/' },
@@ -30,11 +33,16 @@ const items = [
     link: 'https://github.com/panagiotiskon/Extendible-Hashing-for-DBMS',
     description:
       'A low-level implementation in C of extendible hashing for database systems.This method uses directories and buckets to hash data and to maximize flexibility and efficiency in computing time.',
-    header: <Skeleton imageSrc={'/ig.png'} />,
+    header: (
+      <Skeleton
+        imageSrc={'/test2.svg'}
+        otherClassNames=" h-max scale-[2.2]"
+      />
+    ),
     className: 'md:col-span-1',
     skills: [
       {
-        skillName: 'Extendable Hashing',
+        skillName: 'Extendible Hashing',
         skillURL: 'https://en.wikipedia.org/wiki/Extendible_hashing',
       },
     ],
@@ -44,7 +52,12 @@ const items = [
     link: 'https://github.com/panagiotiskon/Software-Development-for-Algorithmic-Problems-Full-Project',
     description:
       'This project implements and optimizes Incremental and Convex Hull algorithms using Local Search, Simulated Annealing, and preprocessing, then applies them to test sets and outputs results for analysis.',
-    header: <Skeleton imageSrc={'/ig.png'} />,
+    header: (
+      <Skeleton
+        imageSrc={'/test3.svg'}
+        otherClassNames=" h-max scale-[1.3] "
+      />
+    ),
     className: 'md:col-span-1',
     skills: [
       { skillName: 'CGAL', skillURL: 'https://www.cgal.org/' },
@@ -60,7 +73,9 @@ const items = [
     link: 'https://github.com/panagiotiskon/Minijava-Compiler',
     description:
       'Compiler for MiniJava, a subset of Java that can be compiled by a full Java compiler like javac. Performs Semantic Analysis on a set of files and then generates intermediate code used by LLVM compiler.',
-    header: <Skeleton imageSrc={'/ig.png'} otherClassNames="opacity-75" />,
+    header: (
+      <Skeleton imageSrc={'/test4.svg'} otherClassNames="scale-[1.1] h-max" />
+    ),
     className: 'md:col-span-2',
     skills: [
       {
