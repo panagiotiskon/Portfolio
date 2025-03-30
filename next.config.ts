d82276165c746114fd82966b4ce9a3cb203186ca
Panagiotis Kontoeidis <panos.kontoeidis@gmail.com>
2025-03-30 19:41:33 +0300

    github pages issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
