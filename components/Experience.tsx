e79cc219a21fc3a83e7586775b49ea3f0ee666b1
Panagiotis Kontoeidis <panos.kontoeidis@gmail.com>
2025-03-31 18:06:54 +0300

    small fix

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -11,13 +11,13 @@ const Experience = () => {
       className="py-24 flex-col flex justify-center items-center h-screen space-y-9 max-w-[1000px] mx-auto"
       id="experience"
     >
-      <div className="w-full min-h-96 absolute fade-mask mt-96">
+      <div className="w-full lg:min-h-screen min-h-96 absolute fade-mask lg:mt-0 mt-96">
         <Image
           src="./footer-grid.svg"
           alt="footer grid"
           layout="fill"
           objectFit="cover"
-          className="w-full h-full lg:opacity-50 opacity:50"
+          className="w-full h-full lg:opacity-60 opacity:50"
         />
       </div>
       <SectionHeader title=".Experience" align="right" className="max-w-4xl" />
