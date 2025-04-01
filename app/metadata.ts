ba4e3bcfcd763b86fce14562a7ef5da8c685547b
Panagiotis Kontoeidis <panos.kontoeidis@gmail.com>
2025-04-01 21:04:16 +0300

    metadata added

--- app/metadata.ts
+++ app/metadata.ts
@@ -1,7 +1,25 @@
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "Panagiotis's Portfolio",
-  description:
-    'Discover my portfolio, featuring my projects, skills, and experience as a developer.',
+  description: "Discover my portfolio, featuring my projects, skills, and experience as a developer.",
+  openGraph: {
+    title: "Panagiotis's Portfolio",
+    description: "Explore my work in web development!",
+    url: "https://panagiotis-kontoeidis.com",
+    siteName: "Panagiotis's Portfolio",
+    images: [
+      {
+        url: "https://panagiotis-kontoeidis.com/PK-preview.png",
+        width: 1200,
+        height: 627,
+        alt: "Preview of Panagiotis's Portfolio",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Panagiotis's Portfolio",
+    description: "Explore my work in web development!",
+    images: ["https://panagiotis-kontoeidis/PK-preview.png"],
+  },
 };
