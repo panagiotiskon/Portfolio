8d72996c1d6931dc08f5576d04a27a9c456ef589
Panagiotis Kontoeidis <panos.kontoeidis@gmail.com>
2025-11-26 19:04:02 +0200

    reduced profile picture size and css optimizations

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -10,18 +10,15 @@ import Image from 'next/image';
 const AboutMe = () => {
   return (
     <section
-      className="relative flex flex-col items-center justify-center min-h-screen space-y-6 max-w-3xl:"
+      className="relative flex flex-col items-center justify-center min-h-screen space-y-6"
       id="about"
     >
       <SectionHeader title="About Me." align="left" className="max-w-4xl" />
-      <div
-        className="max-w-4xl mx-auto flex flex-col md:flex-row items-center 
-                    gap-12 p-1 md:text-3xl text-sm lg:text-3xl font-bold"
-      >
+      <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center gap-12 p-1 font-bold">
         <Spotlight />
         <GlareCard className="flex flex-col items-center justify-center w-72">
           <Image
-            src="./profile.jpg"
+            src="./profile.webp"
             alt="Profile Picture"
             fill
             style={{ objectFit: 'cover' }}
@@ -29,13 +26,17 @@ const AboutMe = () => {
           />
         </GlareCard>
         <div className="md:text-left space-y-4 text-base text-neutral-300 text-justify">
-          <p>{texts.ABOUT_ME_NEW_1}</p>
-          <p>{texts.ABOUT_ME_NEW_2}</p>
-          <p>{texts.ABOUT_ME_NEW_3}</p>
-          <p>{texts.ABOUT_ME_NEW_4}</p>
-          <p>{texts.ABOUT_ME_NEW_5}</p>
-          <p>{texts.ABOUT_ME_NEW_6}</p>
-          <p>{texts.ABOUT_ME_NEW_7}</p>
+          {[
+            texts.ABOUT_ME_NEW_1,
+            texts.ABOUT_ME_NEW_2,
+            texts.ABOUT_ME_NEW_3,
+            texts.ABOUT_ME_NEW_4,
+            texts.ABOUT_ME_NEW_5,
+            texts.ABOUT_ME_NEW_6,
+            texts.ABOUT_ME_NEW_7,
+          ].map((text, index) => (
+            <p key={index}>{text}</p>
+          ))}
         </div>
       </div>
     </section>
--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,41 @@ const nextConfig: NextConfig = {
     ],
   },
   trailingSlash: true,
+  experimental: {
+    optimizeCss: true,
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/api/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
